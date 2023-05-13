55aa8c8328e13d4337eee9a05aea42d3dd375ee9
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-14 03:19:23 +0900

    Add setting DI

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -2,13 +2,19 @@ package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
 import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
+import retrofit2.Retrofit
 
 interface MarsPhotosRepository {
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
 
-class MarsPhotosRepositoryImpl: MarsPhotosRepository {
+// コンストラクタの引数名はこのクラスが知っている名前をつける。
+// MarsPhotosRepositoryImplを参照するのはDefaultAppContainerで,
+// retrofitServiceをコンストラクタに渡しているからといって、MarsPhotosRepositoryImplのコンストラクタの引数名を
+// retrofitServiceにはしない。なぜなら、MarsPhotosRepositoryImplはretrofitServiceなんて知らないから。
+class MarsPhotosRepositoryImpl(private val marsApiService: MarsApiService): MarsPhotosRepository {
     override suspend fun getMarsPhotos(): List<MarsPhoto> {
-        return MarsApi.retrofitService.getPhotos()
+        return marsApiService.getPhotos()
     }
 }
